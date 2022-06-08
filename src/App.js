6307194a8b0f2fd0f05fcdb65f371df0eb426fdf
Thomas Pendleton <thomas6576@yahoo.com>
2022-06-08 09:58:01 -0700

    finsihed project

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tabs-project'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setJobs(data)
+    setLoading(false)
+  }
+
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = jobs[value]
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* {btn container} */}
+        <div className="btn-container">
+          {jobs.map((job, idx) => {
+            return (
+              <button
+                onClick={() => setValue(idx)}
+                className={`job-btn ${value === idx && 'active-btn'}`}
+                key={job.id}
+              >
+                {job.title}
+              </button>
+            )
+          })}
+        </div>
+        {/* {job info} */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, idx) => {
+            return (
+              <div className="job-desc" key={idx}>
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default App
